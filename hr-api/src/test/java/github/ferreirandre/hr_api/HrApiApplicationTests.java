4fbc10342bf25e25c64bbe80f77d671b20cf8cea
ferreirandre <adrecruz15@gmail.com>
2025-10-21 21:04:18 -0300

    starting hr manager project, will use java spring and probably angular

---
+++ hr-api/src/test/java/github/ferreirandre/hr_api/HrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.ferreirandre.hr_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
